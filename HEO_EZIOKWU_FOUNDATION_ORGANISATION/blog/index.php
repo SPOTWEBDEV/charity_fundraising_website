badf922f59614ec35f7201f118b95907a139d736
somto ogbodo <somtoogbodo2007@gmail.com>
2025-04-20 00:34:03 +0100

    landing page fixed

--- HEO_EZIOKWU_FOUNDATION_ORGANISATION/blog/index.php
+++ HEO_EZIOKWU_FOUNDATION_ORGANISATION/blog/index.php
@@ -118,7 +118,7 @@
                                     <?php echo htmlspecialchars($row['author']); ?>
                                 </li> -->
                                 <li>
-                                    <i class="fas fa-user"></i> John Doe
+                                    <i class="fas fa-user"></i> author
                                 </li>
 
 
